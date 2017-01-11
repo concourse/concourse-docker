5185c876d4fac18df259f778937382937a8dd2d3
Joonas Bergius <joonas@digitalocean.com>
2017-01-11 14:38:39 -0500

    Update the key material names to match that of the docs

--- generate-keys.sh
+++ generate-keys.sh
@@ -4,10 +4,10 @@ set -e -u -x
 
 mkdir -p keys/web keys/worker
 
-yes | ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+yes | ssh-keygen -t rsa -f ./keys/web/host_key -N ''
 yes | ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
 
 yes | ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
 
 cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
-cp ./keys/web/tsa_host_key.pub ./keys/worker
+cp ./keys/web/host_key.pub ./keys/worker
